872f490ba3623a9105f16799ae116fdef4636fe5
Attent1 <rafaeldsc22@outlook.com>
2024-04-01 09:51:12 -0300

    Página de partidas

---
+++ src/app/partidas/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/NavBar";
+
+export default function Partidas() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <NavBar active="partidas"/>
+      <h1>Partidas</h1>
+    </main>
+  );
+}
