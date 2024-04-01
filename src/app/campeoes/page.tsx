602cc8814643e40b2c805483a557ba9f735b75fa
Attent1 <rafaeldsc22@outlook.com>
2024-04-01 09:51:06 -0300

    Página de campeões

---
+++ src/app/campeoes/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/NavBar";
+
+export default function Campeoes() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <NavBar active="campeoes"/>
+      <h1>Campeões</h1>
+    </main>
+  );
+}
