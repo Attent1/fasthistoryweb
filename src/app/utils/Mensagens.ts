3d6105779b385d49a9b4e3c84e3e8297fc7fcef0
Attent1 <rafaeldsc22@outlook.com>
2024-04-19 11:30:26 -0300

    mensagems de consistencias

---
+++ src/app/utils/Mensagens.ts
@@ -0,0 +1,9 @@
+const messages = [
+     { campo: 'campeao', mensagem: 'Campeão obrigatório' },
+    // { campo: 'kill', mensagem: 'Informe as kills' },
+    // { campo: 'death', mensagem: 'Informe as deaths' },
+    // { campo: 'assist', mensagem: 'Informe as assists' },
+    // { campo: 'resultado', mensagem: 'Resultado obrigatório' },
+        
+];
+export default messages;
